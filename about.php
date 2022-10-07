e4ad714fe1390169fb23cd6cb0180f477e36d29f
Kalai2773 <87932746+Kalai2773@users.noreply.github.com>
2022-10-07 22:26:37 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,30 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#00FF33">ONLINE FACULTY FEEDBACK SYSTEM</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <h4>				Student Feedback system for College.<br>
+Online Feedback System is web based system which provides a way for colleges to allow students to gives feedback for staff online to improve their teaching. Students are requires to gives feedback using one standard feedback form.
+<br>
+Online Feedback System allows students to select particular subject particular subject and respective teacher to give feedback about teacher and subject.
+<br>
+This Project provides the proper feedback to the teachers about their teaching quality on basis of rating very poor, poor, average, good, very good.
+
+
+</h4> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
